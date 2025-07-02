611697fa7cf6a09c21a50fde2d82609841de96fe
kavindu subhash <70571627+kavindusubhash28@users.noreply.github.com>
2025-07-02 21:03:26 +0530

    fetch api changed to axios

--- src/main/java/com/example/library/model/User.java
+++ src/main/java/com/example/library/model/User.java
@@ -3,6 +3,7 @@ package com.example.library.model;
 import jakarta.persistence.*;
 
 @Entity
+@Table(name = "\"user\"")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
